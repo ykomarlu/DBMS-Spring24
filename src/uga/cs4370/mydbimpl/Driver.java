978efc1ecf4a1af0cfd32979a8ff9875b5c6f941
alexbradshaw <alexanderbradshaw5@gmail.com>
2024-02-17 14:30:39 -0500

    updated queries 1, 2, 3, and 5

--- src/uga/cs4370/mydbimpl/Driver.java
+++ src/uga/cs4370/mydbimpl/Driver.java
@@ -1,5 +1,6 @@
 package uga.cs4370.mydbimpl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import uga.cs4370.mydb.Cell;
@@ -45,35 +46,43 @@ public class Driver {
         takesRel.loadData("./resources/takes_export.csv");
 
 
-        
+
         Relation q1GreaterThan50k = ra.select(instructorRel, row -> row.get(instructorRel.getAttrIndex("salary")).getAsDouble() > 50000);
         Relation q1InCSDept = ra.select(instructorRel, row -> row.get(instructorRel.getAttrIndex("dept_name")).getAsString() == "Comp. Sci.");
         Relation q1Diff50kCS = ra.diff(q1GreaterThan50k, q1InCSDept);
-        Relation query1 = ra.join(q1Diff50kCS, advisorRel);
+        Relation q1InsJoin = ra.join(q1Diff50kCS, advisorRel);
+        Relation q1SmallStuID = ra.select(q1InsJoin, row -> row.get(q1InsJoin.getAttrIndex("stu_id")).getAsInt() < 20000);
 
-        System.out.println("\n\nPrints the difference between Instructors with a Salary greater than 50k and Instructors in Comp. Sci. That result is then natural joined with advisors");
+        Relation query1 = ra.select(q1SmallStuID, row -> row.get(q1SmallStuID.getAttrIndex("name")).getAsString().contains("s"));
+
+        System.out.println("\n\nPrints the difference between Instructors with a Salary greater than 50k and Instructors in Comp. Sci. That result is then natural joined with advisors. We then select all Stu_IDs smaller than 20k and names that contain the letter S.");
         ra.print50(query1);
 
 
 
-        Relation q2ProjectedHistory = ra.select(studentsRel, row -> row.get(studentsRel.getAttrIndex("dept_name")).equals(Cell.val("History")));
+        Relation q2ProjectedHistory = ra.select(studentsRel, row -> row.get(studentsRel.getAttrIndex("dept_name")).equals(Cell.val("Athletics")));
+        Relation q2LessCredits = ra.select(q2ProjectedHistory, row -> row.get(q2ProjectedHistory.getAttrIndex("tot_cred")).getAsInt() < 22);
         Relation q2ProjectedMarketing = ra.select(studentsRel, row -> row.get(studentsRel.getAttrIndex("dept_name")).equals(Cell.val("Marketing")));
-        Relation unionOfProject = ra.union(q2ProjectedHistory, q2ProjectedMarketing);
-        Relation selectedTakes = ra.project(takesRel, List.of("semester","year"));
-        Relation query2 = ra.cartesianProduct(unionOfProject, selectedTakes);
+        Relation q2MoreCredits = ra.select(q2ProjectedMarketing, row -> row.get(q2ProjectedMarketing.getAttrIndex("tot_cred")).getAsInt() > 100);
+        Relation unionOfProject = ra.union(q2LessCredits, q2MoreCredits);
+        Relation q2projectedIns = ra.project(instructorRel, List.of("dept_name", "salary"));
 
-        System.out.println("\n\nUnion of Students in the History and Marketing departments with Cartesian of Semester, Year from Takes");
-        ra.print50(query2);
+        Relation query2 = ra.join(unionOfProject, q2projectedIns);
+
+        System.out.println("\n\nThe Union of Students who are in Athletics with less than 22 credits and Students in Marketing with more than 100 credits. This is then natural joined with Instructors, which has been projected to only display Dept_Name and Salary");
+        query2.print();
 
 
 
         Relation q3ProjectedStu = ra.project(studentsRel, List.of("name", "dept_name"));
         Relation q3ProjectedIns = ra.project(instructorRel, List.of("name", "dept_name"));
         Relation q3Union = ra.union(q3ProjectedStu, q3ProjectedIns);
-        Relation query3 = ra.rename(q3Union, q3Union.getAttrs(), List.of("First Name", "Department"));
+        Relation q3Rename = ra.rename(q3Union, q3Union.getAttrs(), List.of("First Name", "Department"));
+
+        Relation query3 = ra.select(q3Rename, row -> row.get(q3Rename.getAttrIndex("First Name")).getAsString().length() > 8);
 
         System.out.println("\n\nName, Dept_Name of Student U Instructor, which is renamed to be First Name and Department");
-        ra.print50(query3);
+        query3.print();
 
 
 
@@ -86,13 +95,13 @@ public class Driver {
 
 
 
-        Relation studentCredsAbove60 = ra.select(studentsRel, row -> row.get(studentsRel.getAttrIndex("tot_cred")).getAsInt() > 60);
-        Relation takesStuCredNaturalJoin = ra.join(studentCredsAbove60, takesRel);
-        Relation query5 = ra.project(takesStuCredNaturalJoin, List.of("name", "semester", "course_id", "year", "point_grade"));
+        Relation geoStudent = ra.select(studentsRel,((row)->row.get(studentsRel.getAttrIndex("dept_name")).equals(Cell.val("Geology"))));
+        Relation geoInstructor = ra.select(instructorRel,((row)->row.get(instructorRel.getAttrIndex("dept_name")).equals(Cell.val("Geology"))));
+        Relation projectedStudent = ra.project(geoStudent, List.of("name"));
+        Relation projectedInstructor = ra.project(geoInstructor, List.of("name"));
 
-        System.out.println("\n\nName, Semester, Course_ID, Year, Grade for students whos Tot_Cred are greater than 60");
-        ra.print50(query5);
-
+        Relation geoRelation = (ra.union(projectedStudent, projectedInstructor));
+        geoRelation.print();
 
 
     }
