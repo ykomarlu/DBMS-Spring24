fe68b1b425b6eb2df129dcf41501c85569ad1b9d
alexbradshaw <alexanderbradshaw5@gmail.com>
2024-03-19 18:35:28 -0400

    added sql files

---
+++ project2/tables.sql
@@ -0,0 +1,57 @@
+DROP DATABASE IF EXISTS proj2
+CREATE DATABASE proj2
+
+CREATE TABLE Post ( 
+    postId int NOT NULL,
+    userId int NOT NULL, 
+    postDate date NOT NULL, 
+    postText varchar(255) NOT NULL, 
+    primary key(postId), 
+    foreign key(userId) references user(userId) 
+);
+
+CREATE TABLE Heart (
+    postId int NOT NULL,
+    userId int NOT NULL,
+    primary key (userId, postId),
+    foreign key(postId) references post(postId), 
+    foreign key(userId) references user(userId) 
+);
+
+CREATE TABLE Bookmark ( 
+    postId int NOT NULL,
+    userId int NOT NULL,
+    primary key(userId, postId),
+    foreign key(postId) references post(postId), 
+    foreign key(userId) references user(userId) 
+);
+
+
+CREATE TABLE Hashtag ( 
+    hashtag varchar(225) NOT NULL, 
+    postId int NOT NULL, 
+    primary key(hashtag,postId) ,
+    foreign key(postId) references post(postId)
+);
+
+CREATE TABLE Comment (
+    commentId int not null, 
+    postId int not null, 
+    userId int not null,
+    commentDate date not null,
+    commentText varchar(225) not null,
+    primary key(commentId),
+    foreign key (postId) References Post(postId),
+    foreign key (userId) references user(userId)
+);
+
+
+CREATE TABLE Follow ( 
+    followerUserId int NOT NULL, 
+    followeeUserId int NOT NULL, 
+    primary key (followerUserId, followeeUserId), 
+    foreign key(followerUserId) references user(userId), 
+    foreign key(followeeUserId) references user(userId) 
+);
+
+-- Order of Creating Tables: User, Post, Hashtag, comment, bookmark, heart, follow
