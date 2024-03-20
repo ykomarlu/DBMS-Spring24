fea356baeaf977d4a663b3182fc0572ef4f44837
alexbradshaw <31080674+alexbradshaw@users.noreply.github.com>
2024-03-20 19:31:01 -0400

    moved our sql to his db file

--- project2/database_setup.sql
+++ project2/database_setup.sql
@@ -17,3 +17,58 @@ create table if not exists user (
     constraint firstName_min_length check (char_length(trim(firstName)) >= 2),
     constraint lastName_min_length check (char_length(trim(lastName)) >= 2)
 );
+
+CREATE TABLE Post ( 
+    postId int NOT NULL,
+    userId int NOT NULL, 
+    postDate date NOT NULL, 
+    postText varchar(255) NOT NULL, 
+    primary key(postId), 
+    foreign key(userId) references user(userId) 
+);
+
+CREATE TABLE Heart (
+    postId int NOT NULL,
+    userId int NOT NULL,
+    primary key (userId, postId),
+    foreign key(postId) references post(postId), 
+    foreign key(userId) references user(userId) 
+);
+
+CREATE TABLE Bookmark ( 
+    postId int NOT NULL,
+    userId int NOT NULL,
+    primary key(userId, postId),
+    foreign key(postId) references post(postId), 
+    foreign key(userId) references user(userId) 
+);
+
+
+CREATE TABLE Hashtag ( 
+    hashtag varchar(225) NOT NULL, 
+    postId int NOT NULL, 
+    primary key(hashtag,postId) ,
+    foreign key(postId) references post(postId)
+);
+
+CREATE TABLE Comment (
+    commentId int not null, 
+    postId int not null, 
+    userId int not null,
+    commentDate date not null,
+    commentText varchar(225) not null,
+    primary key(commentId),
+    foreign key (postId) References Post(postId),
+    foreign key (userId) references user(userId)
+);
+
+
+CREATE TABLE Follow ( 
+    followerUserId int NOT NULL, 
+    followeeUserId int NOT NULL, 
+    primary key (followerUserId, followeeUserId), 
+    foreign key(followerUserId) references user(userId), 
+    foreign key(followeeUserId) references user(userId) 
+);
+
+-- Order of Creating Tables: User, Post, Hashtag, comment, bookmark, heart, follow
